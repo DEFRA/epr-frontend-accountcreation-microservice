692f2bbd63852f2dd6990bf21697e004255b98cc
tim-powell-eviden <156194496+tim-powell-eviden@users.noreply.github.com>
2024-06-21 11:29:12 +0100

    Feature/386670 footer update (#6)
    * Update the privacy and accessibility urls in the footer
    
    * Remove the unneeded accesibility page, since we now link to an URL external to this project instead
    
    * Change privacy & accessibility links to open in new tabs
    
    Remove returnurl decorator
    
    * Add customer service contact details to footer
    
    Welsh translation for opening times is to be determined
    
    * Replace the placeholder text for the welsh translation for the footer with the final text
    
    * Fix Welsh email text and copyright symbol not being displayed
    
    * Correct more capitalisation

--- src/FrontendAccountCreation.Web/Configs/ExternalUrlsOptions.cs
+++ src/FrontendAccountCreation.Web/Configs/ExternalUrlsOptions.cs
@@ -47,4 +47,10 @@ public class ExternalUrlsOptions
     public string PrivacyLink { get; set; }
     
     public string CookiesLink { get; set; }
+
+    public string AccessibilityLink { get; set; }
+
+    public string CustomerServiceEmail { get; set; }
+
+    public string CustomerServicePhone { get; set; }
 }
