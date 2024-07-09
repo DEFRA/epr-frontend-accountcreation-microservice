1c00b889ce6c11d16337d77c20a892a7953a485b
Simon Crowder (ext) <155532504+scdefra@users.noreply.github.com>
2024-07-09 12:01:13 +0100

    Added cookie controller tests

---
+++ src/FrontendAccountCreation.IntegrationTests/Resources/TestRequestCookieCollection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontendAccountCreation.IntegrationTests.Resources
+{
+    public class TestRequestCookieCollection : Dictionary<string, string>, IRequestCookieCollection
+    {
+        public new ICollection<string> Keys => base.Keys;
+        public new string this[string key]
+        {
+            get
+            {
+                TryGetValue(key, out var value);
+                return value;
+            }
+            set
+            {
+                base[key] = value;
+            }
+        }
+    }
+}
