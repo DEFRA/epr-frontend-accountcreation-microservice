80b868ba150fdb23a600d528edda8e8e32f21b95
EvidenPete <155994511+EvidenPete@users.noreply.github.com>
2024-09-18 07:53:03 +0100

    initial version

---
+++ src/FrontendAccountCreation.Web/ViewModels/Shared/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿using FrontendAccountCreation.Web.Extensions;
+
+namespace FrontendAccountCreation.Web.ViewModels.Shared
+{
+    public class ErrorViewModel
+    {
+        public string Timestamp { get; set; } = DateTime.UtcNow.UtcToGmt().ToString();
+    }
+}
