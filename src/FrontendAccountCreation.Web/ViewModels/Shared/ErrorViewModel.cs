cfced7a8052b50c81d51e533a00f7a9873587e3f
EvidenPete <155994511+EvidenPete@users.noreply.github.com>
2024-09-24 20:38:14 +0100

    error page fix

---
+++ src/FrontendAccountCreation.Web/ViewModels/Shared/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+﻿using FrontendAccountCreation.Web.Extensions;
+
+namespace FrontendAccountCreation.Web.ViewModels.Shared;
+
+public class ErrorViewModel
+{
+    public string Timestamp { get; set; } = DateTime.UtcNow.UtcToGmt().ToString("dd/MM/yyyy HH:mm:ss");
+}
