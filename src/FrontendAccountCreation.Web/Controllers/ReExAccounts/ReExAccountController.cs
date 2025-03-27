007de78d2064d934d9968b3cd35365b4ac1fa721
Sharjeel Baloch <sharjeel.baloch.external@eviden.com>
2025-03-27 17:29:38 +0000

    Added ReprocessorExporter  folder/controller for accout creation

---
+++ src/FrontendAccountCreation.Web/Controllers/ReExAccounts/ReExAccountController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FrontendAccountCreation.Web.Controllers.ReExAccounts
+{
+    /// <summary>
+    /// Reprocessor & Exporter Account creation controller.
+    /// </summary>
+    public class ReExAccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
