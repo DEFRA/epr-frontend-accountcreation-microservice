e8c96df814938b12d1bae39fd774aa69fe2608e5
Phil Davies <203141900+phil-davies-eviden@users.noreply.github.com>
2025-04-09 10:24:36 +0100

    unit tests wip

--- src/FrontendAccountCreation.Web.UnitTests/Middleware/ReExJourneyAccessCheckerMiddlewareTests.cs
+++ src/FrontendAccountCreation.Web.UnitTests/Middleware/ReExJourneyAccessCheckerMiddlewareTests.cs
@@ -9,6 +9,11 @@ using Moq;
 
 namespace FrontendAccountCreation.Web.UnitTests.Middleware;
 
+/// <summary>
+/// Access means a page in the journey with the journey attribute.
+/// No Access means the first page in the journey without the journey attribute.
+/// Naming could be better, but we match JourneyAccessCheckerMiddlewareTests for consistency.
+/// </summary>
 [TestClass]
 public class ReExJourneyAccessCheckerMiddlewareTests
 {
@@ -92,8 +97,6 @@ public class ReExJourneyAccessCheckerMiddlewareTests
         _httpResponseMock.Verify(x => x.Redirect(firstPageUrl), Times.Once);
     }
 
-    //todo: better name?
-    // no access means the first page in the journey (without the journey attribute)
     [TestMethod]
     [DataRow(PagePath.FullName)]
     public async Task GivenNoAccessRequiredPage_WhenInvokeCalled_ThenNoRedirectionHappened(string pageUrl)
@@ -107,24 +110,6 @@ public class ReExJourneyAccessCheckerMiddlewareTests
         // Assert
         _httpResponseMock.Verify(x => x.Redirect(It.IsAny<string>()), Times.Never);
     }
-
-#if check_if_needed
-    [TestMethod]
-    public async Task GivenBusinessAddressPage_WhenInvokeCalled_ThenNoRedirectionOccurs()
-    {
-        // Arrange
-        var session = new ReExAccountCreationSession { Journey = { PagePath.SelectBusinessAddress } };
-        _sessionManagerMock.Setup(x => x.GetSessionAsync(It.IsAny<ISession>())).ReturnsAsync(session);
-
-        SetupEndpointMock(new ReprocessorExporterJourneyAccessAttribute(PagePath.BusinessAddress));
-
-        // Act
-        await _middleware.Invoke(_httpContextMock.Object, _sessionManagerMock.Object);
-
-        // Assert
-        _httpResponseMock.Verify(x => x.Redirect(It.IsAny<string>()), Times.Never);
-    }
-#endif
 
     private void SetupEndpointMock(params object[] attributes)
     {
