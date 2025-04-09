b47de7b904fa88c6d764a9adf6b4a63d907fa9f0
Sharjeel Baloch <sharjeel.baloch.external@eviden.com>
2025-04-09 19:05:46 +0100

    Feature/529932 add companies house number (#51)
    * Added Companies house number and related unit tests
    Added middleware for Organisation and organisationSession etc.
    
    * Added organisationMapper and related unit tests

---
+++ src/FrontendAccountCreation.Web/Controllers/Attributes/OrganisationJourneyAccessAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace FrontendAccountCreation.Web.Controllers.Attributes;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
+public class OrganisationJourneyAccessAttribute : Attribute
+{
+    public OrganisationJourneyAccessAttribute(string pagePath)
+    {
+        PagePath = pagePath;
+    }
+
+    public string PagePath { get; }
+}
