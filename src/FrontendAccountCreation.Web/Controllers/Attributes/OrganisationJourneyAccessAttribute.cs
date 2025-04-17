7028b0fcec966b3a884c5418d5ece5e4c9586f20
Phil Davies <203141900+phil-davies-eviden@users.noreply.github.com>
2025-04-17 15:51:48 +0100

    add comment only

--- src/FrontendAccountCreation.Web/Controllers/Attributes/OrganisationJourneyAccessAttribute.cs
+++ src/FrontendAccountCreation.Web/Controllers/Attributes/OrganisationJourneyAccessAttribute.cs
@@ -1,6 +1,7 @@
 ﻿namespace FrontendAccountCreation.Web.Controllers.Attributes;
 
 // we might have to expand the feature handling to accept a collection (or collection for and/or handling), but we don't need it yet
+// we don't strictly need to explicitly specify the requiredFeature, but this moves us closer to generic components to handle the journey
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
 public class OrganisationJourneyAccessAttribute(string pagePath, string? requiredFeature = null) : Attribute
 {
