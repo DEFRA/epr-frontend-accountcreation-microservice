b4a091c7dd11cce6967f1beaeef6b967bfc99c09
Stephen Williamson <stephen.williamson@defra.onmicrosoft.com>
2025-04-22 17:10:25 +0100

    Remove unused properties to keep Sonarqube happy

--- src/FrontendAccountCreation.Core/Sessions/ReEx/ReExCompanyTeamMember.cs
+++ src/FrontendAccountCreation.Core/Sessions/ReEx/ReExCompanyTeamMember.cs
@@ -2,12 +2,6 @@
 
 public class ReExCompanyTeamMember
 {
-    public string FullName { get; set; }
-
-    public string Email  { get; set; }
-
-    public string Telephone { get; set; }
-
     public bool IsInvited { get; set; }
 
     public ReExTeamMemberRole? Role { get; set; }
