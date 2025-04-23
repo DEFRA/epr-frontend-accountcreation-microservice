f921ef05e6f43f9e84ef4ac77f3b1c2cf7bcd248
Sharjeel Baloch <sharjeel.baloch.external@eviden.com>
2025-04-23 09:26:55 +0100

    Feature/539577 is partner organisation unit tests (#68)
    * 539577 - added is partner Organisation question
    
    * 539577-Added related unit test

--- src/FrontendAccountCreation.Web/Controllers/ReprocessorExporter/OrganisationController.cs
+++ src/FrontendAccountCreation.Web/Controllers/ReprocessorExporter/OrganisationController.cs
@@ -302,7 +302,7 @@ public class OrganisationController : Controller
 
         if (session.IsOrganisationAPartnership == true)
         {
-            // TODO: Yes or No ending up same pagePath
+            // TODO: Yes or No ending up same pagePath - to be confirmed
             return await SaveSessionAndRedirect(session, nameof(RoleInOrganisation), PagePath.IsPartnership, PagePath.RoleInOrganisation);
         }
         return await SaveSessionAndRedirect(session, nameof(IsOrganisationAPartner), PagePath.IsPartnership, PagePath.RoleInOrganisation);
