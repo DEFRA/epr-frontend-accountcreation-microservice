27f9bfdf2b0f7e536b5244cdb4a78a4aeb70ac26
Phil Davies <203141900+phil-davies-eviden@users.noreply.github.com>
2025-04-24 13:14:28 +0100

    team role page wip

---
+++ src/FrontendAccountCreation.Core/Sessions/TeamRoleInOrganisation.cs
@@ -0,0 +1,9 @@
+﻿namespace FrontendAccountCreation.Core.Sessions;
+
+//todo: or call it ApprovedPersonRoleInOrganisation
+public enum TeamRoleInOrganisation
+{
+    NoneOfTheAbove = 0,
+    Director = 1,
+    CompanySecretary = 2
+}
--- src/FrontendAccountCreation.Web/ViewModels/ReExAccount/TeamRoleViewModel.cs
+++ src/FrontendAccountCreation.Web/ViewModels/ReExAccount/TeamRoleViewModel.cs
@@ -1,5 +1,10 @@
-﻿namespace FrontendAccountCreation.Web.ViewModels.ReExAccount;
+﻿using FrontendAccountCreation.Core.Sessions;
+using System.ComponentModel.DataAnnotations;
+
+namespace FrontendAccountCreation.Web.ViewModels.ReExAccount;
 
 public class TeamRoleViewModel
 {
+    [Required(ErrorMessage = "TeamRole.ErrorMessage")]
+    public TeamRoleInOrganisation? TeamRoleInOrganisation { get; set; }
 }
