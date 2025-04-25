cfbf09213037575ada761d7638c92bb0e3795703
Phil Davies <203141900+phil-davies-eviden@users.noreply.github.com>
2025-04-25 15:05:14 +0100

    user creation title and header wip

---
+++ src/FrontendAccountCreation.Web/Constants/LayoutOverrides.cs
@@ -0,0 +1,12 @@
+﻿namespace FrontendAccountCreation.Web.Constants;
+
+public static class LayoutOverrides
+{
+    public const string CreateAccountTitleOverride = "CreateAnAccount";
+
+    public const string CreateUserSuccessTitleOverride = "ReExApplicationTitle";
+    public const string CreateUserSuccessHeaderOverride = "_ReExHeader";
+
+    public const string OrganisationTitleOverride = "ReExApplicationTitle";
+    public const string OrganisationHeaderOverride = "_ReExHeader";
+}
