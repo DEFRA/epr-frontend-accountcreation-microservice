655aea7812dc4e1fbcf5eb3510e594eb30b920a4
Phil Davies <203141900+phil-davies-eviden@users.noreply.github.com>
2025-04-28 13:47:02 +0100

    it does

--- src/FrontendAccountCreation.Web/Controllers/Attributes/FeatureAttribute.cs
+++ src/FrontendAccountCreation.Web/Controllers/Attributes/FeatureAttribute.cs
@@ -1,6 +1,5 @@
 ﻿namespace FrontendAccountCreation.Web.Controllers.Attributes;
 
-//todo: check works on method too
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
 public class FeatureAttribute(string? requiredFeature = null) : Attribute
 {
