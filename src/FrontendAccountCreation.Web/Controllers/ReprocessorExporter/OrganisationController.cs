fb44ad61b62ac1ba8d28ab810b34571758527b35
Phil Davies <203141900+phil-davies-eviden@users.noreply.github.com>
2025-05-02 13:58:04 +0100

    sonar is once again complaining about lack of code coverage on an unchanged file

--- src/FrontendAccountCreation.Web/Controllers/ReprocessorExporter/OrganisationController.cs
+++ src/FrontendAccountCreation.Web/Controllers/ReprocessorExporter/OrganisationController.cs
@@ -615,6 +615,7 @@ public class OrganisationController : Controller
         return Ok();
     }
 
+    [ExcludeFromCodeCoverage]
     public void NotImplementedMethod()
     {
         // TO DO following & modify - once Tungsten has merged
