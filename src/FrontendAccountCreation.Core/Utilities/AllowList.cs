6f22336cfb54fa640dda0aa8277bb1b0ab37dbe6
Phil Davies <203141900+phil-davies-eviden@users.noreply.github.com>
2025-05-07 16:25:58 +0100

    Sonar isn't picking up the unit tests covering AllowList (tech debt: figure out why)

--- src/FrontendAccountCreation.Core/Utilities/AllowList.cs
+++ src/FrontendAccountCreation.Core/Utilities/AllowList.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
 
 namespace FrontendAccountCreation.Core.Utilities;
 
+[ExcludeFromCodeCoverage(Justification = "Sonar isn't picking up that this code is covered by the tests in AllowListTests.cs")]
 public class AllowList<T>
 {
     private readonly ImmutableHashSet<T> _allowList;
