e585a572d9d11f3846881ad4e56423ed35ca6d55
Ravi Sharma Gautam <ravi.sharmagautam@defradev.onmicrosoft.com>
2025-05-12 17:01:58 +0100

    Added Journey access for approved person for stopping them from jumping questions

--- src/FrontendAccountCreation.Web/Controllers/ReprocessorExporter/ApprovedPersonController.cs
+++ src/FrontendAccountCreation.Web/Controllers/ReprocessorExporter/ApprovedPersonController.cs
@@ -2,12 +2,14 @@
 using FrontendAccountCreation.Core.Sessions;
 using FrontendAccountCreation.Core.Sessions.ReEx;
 using FrontendAccountCreation.Web.Constants;
+using FrontendAccountCreation.Web.Controllers.Attributes;
 using FrontendAccountCreation.Web.Sessions;
 using FrontendAccountCreation.Web.ViewModels.ReExAccount;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FrontendAccountCreation.Web.Controllers.ReprocessorExporter
 {
+    [Feature(FeatureFlags.AddOrganisationCompanyHouseDirectorJourney)]
     [Route("re-ex/organisation")]
     public partial class ApprovedPersonController : Controller
     {
@@ -20,6 +22,7 @@ namespace FrontendAccountCreation.Web.Controllers.ReprocessorExporter
 
         [HttpGet]
         [Route(PagePath.AddAnApprovedPerson)]
+        [OrganisationJourneyAccess(PagePath.AddAnApprovedPerson)]
         public async Task<IActionResult> AddApprovedPerson()
         {
             var session = await _sessionManager.GetSessionAsync(HttpContext.Session);
@@ -30,8 +33,10 @@ namespace FrontendAccountCreation.Web.Controllers.ReprocessorExporter
 
         [HttpPost]
         [Route(PagePath.AddAnApprovedPerson)]
+        [OrganisationJourneyAccess(PagePath.AddAnApprovedPerson)]
         public async Task<IActionResult> AddApprovedPerson(AddApprovedPersonViewModel model)
         {
+            var session = await _sessionManager.GetSessionAsync(HttpContext.Session);
             if (!ModelState.IsValid)
             {
                 return View(model);
@@ -44,7 +49,8 @@ namespace FrontendAccountCreation.Web.Controllers.ReprocessorExporter
 
             if (model.InviteUserOption == InviteUserOptions.InviteAnotherPerson.ToString())
             {
-                return RedirectToAction(nameof(TeamMemberRoleInOrganisation));
+                return await SaveSessionAndRedirect(session, nameof(TeamMemberRoleInOrganisation),
+                    PagePath.AddAnApprovedPerson, PagePath.TeamMemberRoleInOrganisation);
             }
 
             // I-will-Invite-an-Approved-Person-Later
@@ -53,6 +59,7 @@ namespace FrontendAccountCreation.Web.Controllers.ReprocessorExporter
 
         [HttpGet]
         [Route(PagePath.TeamMemberRoleInOrganisation)]
+        [OrganisationJourneyAccess(PagePath.TeamMemberRoleInOrganisation)]
         public async Task<IActionResult> TeamMemberRoleInOrganisation([FromQuery] Guid? id)
         {
             OrganisationSession session = await _sessionManager.GetSessionAsync(HttpContext.Session);
@@ -78,6 +85,7 @@ namespace FrontendAccountCreation.Web.Controllers.ReprocessorExporter
 
         [HttpPost]
         [Route(PagePath.TeamMemberRoleInOrganisation)]
+        [OrganisationJourneyAccess(PagePath.TeamMemberRoleInOrganisation)]
         public async Task<IActionResult> TeamMemberRoleInOrganisation(TeamMemberRoleInOrganisationViewModel model)
         {
             OrganisationSession? session = await _sessionManager.GetSessionAsync(HttpContext.Session);
@@ -97,7 +105,6 @@ namespace FrontendAccountCreation.Web.Controllers.ReprocessorExporter
                 queryStringId = model.Id.Value;
                 isExistingMember = true;
                 session.ReExCompaniesHouseSession.TeamMembers[index.Value].Role = model.RoleInOrganisation;
-
             }
             else
             {
@@ -110,28 +117,36 @@ namespace FrontendAccountCreation.Web.Controllers.ReprocessorExporter
                 session.ReExCompaniesHouseSession = companiesHouseSession;
             }
 
-            session.IsUserChangingDetails = false;
-            await SaveSession(session, PagePath.TeamMemberRoleInOrganisation, PagePath.TeamMemberDetails);
-
             if (isExistingMember)
             {
                 // move to enter team member details: full name, email, telephone
-                return RedirectToAction(nameof(TeamMembersCheckInvitationDetails));
+                return await SaveSessionAndRedirect(session, nameof(TeamMembersCheckInvitationDetails),
+                    $"{PagePath.TeamMemberRoleInOrganisation}?id={queryStringId}",
+                    PagePath.TeamMembersCheckInvitationDetails
+                    );
             }
             else
             {
-                // go back to check their invitation detials
-                return RedirectToAction(nameof(TeamMemberDetails), new { id = queryStringId });
+                //go back to check their invitation detials
+            return await SaveSessionAndRedirect(session, nameof(TeamMemberDetails),
+                $"{PagePath.TeamMemberRoleInOrganisation}",
+                $"{PagePath.TeamMemberDetails}?id={queryStringId}",
+                new { id = queryStringId });
             }
         }
 
         [HttpGet]
         [Route(PagePath.TeamMemberDetails)]
+        [OrganisationJourneyAccess(PagePath.TeamMemberDetails)]
         public async Task<IActionResult> TeamMemberDetails([FromQuery] Guid id)
         {
+            if (id == Guid.Empty)
+            {
+                return RedirectToAction(nameof(TeamMemberRoleInOrganisation));
+            }
             var session = await _sessionManager.GetSessionAsync(HttpContext.Session);
             await _sessionManager.SaveSessionAsync(HttpContext.Session, session);
-            
+
             var index = session.ReExCompaniesHouseSession?.TeamMembers?.FindIndex(0, x => x.Id.Equals(id));
 
             if (index is >= 0)
@@ -152,6 +167,7 @@ namespace FrontendAccountCreation.Web.Controllers.ReprocessorExporter
 
         [HttpPost]
         [Route(PagePath.TeamMemberDetails)]
+        [OrganisationJourneyAccess(PagePath.TeamMemberDetails)]
         public async Task<IActionResult> TeamMemberDetails(TeamMemberViewModel model)
         {
             var session = await _sessionManager.GetSessionAsync(HttpContext.Session);
@@ -172,7 +188,7 @@ namespace FrontendAccountCreation.Web.Controllers.ReprocessorExporter
             }
 
             // go to check invitation details summary page for all team members
-            return await SaveSessionAndRedirect(session, nameof(TeamMembersCheckInvitationDetails), PagePath.TeamMemberDetails,
+            return await SaveSessionAndRedirect(session, nameof(TeamMembersCheckInvitationDetails), $"{PagePath.TeamMemberDetails}?id={model.Id}",
                 PagePath.TeamMembersCheckInvitationDetails);
         }
 
@@ -183,6 +199,7 @@ namespace FrontendAccountCreation.Web.Controllers.ReprocessorExporter
         /// <returns></returns>
         [HttpGet]
         [Route(PagePath.TeamMembersCheckInvitationDetails)]
+        [OrganisationJourneyAccess(PagePath.TeamMembersCheckInvitationDetails)]
         public async Task<IActionResult> TeamMembersCheckInvitationDetails([FromQuery] Guid? id)
         {
             OrganisationSession session = await _sessionManager.GetSessionAsync(HttpContext.Session);
@@ -203,18 +220,21 @@ namespace FrontendAccountCreation.Web.Controllers.ReprocessorExporter
         }
 
         private async Task<RedirectToActionResult> SaveSessionAndRedirect(OrganisationSession session,
-            string actionName, string currentPagePath, string? nextPagePath)
+            string actionName, string currentPagePath, string? nextPagePath, object? routeValues = null)
         {
             session.IsUserChangingDetails = false;
             await SaveSession(session, currentPagePath, nextPagePath);
 
-            return RedirectToAction(actionName);
+            return RedirectToAction(actionName, routeValues);
         }
 
         private async Task SaveSession(OrganisationSession session, string currentPagePath, string? nextPagePath)
         {
             ClearRestOfJourney(session, currentPagePath);
-
+            if (nextPagePath == PagePath.TeamMembersCheckInvitationDetails)
+            {
+                session.Journey.AddIfNotExists(PagePath.TeamMemberDetails);
+            }
             session.Journey.AddIfNotExists(nextPagePath);
 
             await _sessionManager.SaveSessionAsync(HttpContext.Session, session);
@@ -222,7 +242,7 @@ namespace FrontendAccountCreation.Web.Controllers.ReprocessorExporter
 
         private static void ClearRestOfJourney(OrganisationSession session, string currentPagePath)
         {
-            var index = session.Journey.IndexOf(currentPagePath);
+            var index = session.Journey.FindIndex(x => x.Contains(currentPagePath.Split("?")[0]));
 
             // this also cover if current page not found (index = -1) then it clears all pages
             session.Journey = session.Journey.Take(index + 1).ToList();
