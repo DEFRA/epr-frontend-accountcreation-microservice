007bd39b9a450fa48416bed56567158081f30f74
Ehsan Hatami <ehsan.hatamialamdari.external@eviden.com>
2025-05-12 18:18:58 +0100

    cleanup

--- src/FrontendAccountCreation.Core/Sessions/ReEx/LimitedPartnership/TeamMemberRole.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FrontendAccountCreation.Core.Sessions.ReEx.LimitedPartnership;
-public enum TeamMemberRole
-{
-    None = 0,
-    IndividualPartner = 1,
-    Director = 2,
-    CompanySecretary = 3
-}
