3c5a52f803482fb69f853cdd10d1cfb15b61c779
Venkata Rangala <venkata.rangala.external@eviden.com>
2025-05-16 15:12:23 +0100

    Unit tests addition for LimitedPartnershipRole

--- src/FrontendAccountCreation.Web/ViewModels/ReExAccount/LimitedPartnershipRoleViewModel.cs
+++ src/FrontendAccountCreation.Web/ViewModels/ReExAccount/LimitedPartnershipRoleViewModel.cs
@@ -1,10 +1,8 @@
 ﻿using FrontendAccountCreation.Core.Sessions;
 using System.ComponentModel.DataAnnotations;
-using System.Diagnostics.CodeAnalysis;
 
 namespace FrontendAccountCreation.Web.ViewModels.ReExAccount;
 
-[ExcludeFromCodeCoverage(Justification = "Get feature branch into testing")]
 public class LimitedPartnershipRoleViewModel
 {
     [Required(ErrorMessage = "LimitedPartnershipRole.ErrorMessage")]
