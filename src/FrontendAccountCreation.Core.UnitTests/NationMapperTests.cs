d53e71fa725a54c8304f06327d5c91d3a437dd52
mkeviden <156194524+mkeviden@users.noreply.github.com>
2025-06-09 17:17:50 +0100

    #559333 fix to unit test

--- src/FrontendAccountCreation.Core.UnitTests/NationMapperTests.cs
+++ src/FrontendAccountCreation.Core.UnitTests/NationMapperTests.cs
@@ -15,7 +15,8 @@ namespace FrontendAccountCreation.Core.UnitTests
         [DataRow("2", Nation.NorthernIreland)]
         [DataRow("3", Nation.Scotland)]
         [DataRow("4", Nation.Wales)]
-        
+        [DataRow("0", Nation.NotSet)]
+
         public void TryMapToNation_ValidInput_ReturnsTrueAndCorrectEnum(string input, Nation expected)
         {
             // Act
