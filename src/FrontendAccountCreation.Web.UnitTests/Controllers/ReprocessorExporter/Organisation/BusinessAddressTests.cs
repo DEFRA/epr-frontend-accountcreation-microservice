33369b9799286a21fe700411dc5635552f088ae8
Phil Davies <203141900+phil-davies-eviden@users.noreply.github.com>
2025-06-12 16:04:21 +0100

    unit tests wip

---
+++ src/FrontendAccountCreation.Web.UnitTests/Controllers/ReprocessorExporter/Organisation/BusinessAddressTests.cs
@@ -0,0 +1,207 @@
+﻿using FluentAssertions;
+using FrontendAccountCreation.Core.Sessions;
+using FrontendAccountCreation.Core.Sessions.ReEx;
+using FrontendAccountCreation.Web.Constants;
+using FrontendAccountCreation.Web.Controllers.ReprocessorExporter;
+using FrontendAccountCreation.Web.ViewModels.AccountCreation;
+using FrontendAccountCreation.Web.ViewModels.ReExAccount;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace FrontendAccountCreation.Web.UnitTests.Controllers.ReprocessorExporter.Organisation;
+
+[TestClass]
+public class BusinessAddressTests : OrganisationTestBase
+{
+    private OrganisationSession _organisationSession = null!;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        SetupBase();
+
+        _organisationSession = new OrganisationSession
+        {
+            Journey =
+            [
+                //todo:
+                PagePath.RegisteredAsCharity, PagePath.RegisteredWithCompaniesHouse, PagePath.CompaniesHouseNumber,
+                PagePath.ConfirmCompanyDetails, PagePath.UkNation, PagePath.IsTradingNameDifferent,
+                PagePath.TradingName
+            ]
+        };
+
+        _sessionManagerMock.Setup(x => x.GetSessionAsync(It.IsAny<ISession>())).ReturnsAsync(_organisationSession);
+    }
+
+    [TestMethod]
+    public async Task GET_WhenTradingNameIsNotInSession_ThenViewIsReturnedWithoutDetails()
+    {
+        //Act
+        var result = await _systemUnderTest.BusinessAddress();
+
+        //Assert
+        result.Should().BeOfType<ViewResult>();
+        var viewResult = (ViewResult)result;
+        viewResult.Model.Should().BeOfType<ReExBusinessAddressViewModel>();
+        var viewModel = (ReExBusinessAddressViewModel?)viewResult.Model;
+        viewModel!.BuildingNumber.Should().BeNull();
+        viewModel.BuildingName.Should().BeNull();
+        viewModel.Street.Should().BeNull();
+        viewModel.Town.Should().BeNull();
+        viewModel.County.Should().BeNull();
+        viewModel.Postcode.Should().BeNull();
+    }
+
+    //[TestMethod]
+    //public async Task GET_WhenTradingNameIsInSession_ThenViewIsReturnedWithTradingName()
+    //{
+    //    //Arrange
+    //    const string tradingName = "Trading name";
+    //    _organisationSession.ReExManualInputSession = new ReExManualInputSession
+    //    {
+    //        TradingName = tradingName,
+    //        BusinessAddress = new Core.Addresses.Address()
+    //    };
+
+    //    //Act
+    //    var result = await _systemUnderTest.TradingName();
+
+    //    //Assert
+    //    result.Should().BeOfType<ViewResult>();
+    //    var viewResult = (ViewResult)result;
+    //    viewResult.Model.Should().BeOfType<TradingNameViewModel>();
+    //    var viewModel = (TradingNameViewModel?)viewResult.Model;
+    //    viewModel!.TradingName.Should().Be(tradingName);
+    //}
+
+    //[TestMethod]
+    //public async Task GET_ThenBackLinkIsCorrect()
+    //{
+    //    //Act
+    //    var result = await _systemUnderTest.TradingName();
+
+    //    //Assert
+    //    result.Should().BeOfType<ViewResult>();
+    //    var viewResult = (ViewResult)result;
+    //    AssertBackLink(viewResult, PagePath.IsTradingNameDifferent);
+    //}
+
+    //[TestMethod]
+    //public async Task POST_GivenTradingName_CompaniesHouseFlow_ThenRedirectToPartnerOrganisation()
+    //{
+    //    // Arrange
+    //    var request = new TradingNameViewModel { TradingName = "John Brown Greengrocers" };
+    //    _organisationSession.OrganisationType = OrganisationType.CompaniesHouseCompany;
+
+    //    // Act
+    //    var result = await _systemUnderTest.TradingName(request);
+
+    //    // Assert
+    //    result.Should().BeOfType<RedirectToActionResult>();
+
+    //    ((RedirectToActionResult)result).ActionName.Should().Be(nameof(OrganisationController.IsOrganisationAPartner));
+    //}
+
+    //[TestMethod]
+    //public async Task POST_GivenTradingName_NonCompaniesHouseFlow_ThenRedirectToTypeOfOrganisation()
+    //{
+    //    // Arrange
+    //    var request = new TradingNameViewModel { TradingName = "John Brown Greengrocers" };
+    //    _organisationSession.OrganisationType = OrganisationType.NonCompaniesHouseCompany;
+
+    //    // Act
+    //    var result = await _systemUnderTest.TradingName(request);
+
+    //    // Assert
+    //    result.Should().BeOfType<RedirectToActionResult>();
+
+    //    ((RedirectToActionResult)result).ActionName.Should().Be(nameof(OrganisationController.TypeOfOrganisation));
+    //}
+
+    //[TestMethod]
+    //public async Task POST_GivenTradingName_ThenUpdatesSession()
+    //{
+    //    // Arrange
+    //    var request = new TradingNameViewModel { TradingName = "John Brown Greengrocers" };
+
+    //    // Act
+    //    await _systemUnderTest.TradingName(request);
+
+    //    // Assert
+    //    _sessionManagerMock.Verify(x => x.SaveSessionAsync(It.IsAny<ISession>(), It.IsAny<OrganisationSession>()), Times.Once);
+    //}
+
+    //[TestMethod]
+    //public async Task POST_GivenNoTradingName_ThenSessionNotUpdated()
+    //{
+    //    // Arrange
+    //    _systemUnderTest.ModelState.AddModelError(nameof(TradingNameViewModel.TradingName), "Trading name field is required");
+
+    //    // Act
+    //    var result = await _systemUnderTest.TradingName(new TradingNameViewModel());
+
+    //    // Assert
+    //    _sessionManagerMock.Verify(x => x.UpdateSessionAsync(It.IsAny<ISession>(), It.IsAny<Action<OrganisationSession>>()),
+    //        Times.Never);
+    //}
+
+    //[TestMethod]
+    //public async Task POST_GivenNoTradingName_ThenReturnView()
+    //{
+    //    // Arrange
+    //    _systemUnderTest.ModelState.AddModelError(nameof(TradingNameViewModel.TradingName), "Trading name field is required");
+    //    var viewModel = new TradingNameViewModel
+    //    {
+    //        TradingName = ""
+    //    };
+
+    //    // Act
+    //    var result = await _systemUnderTest.TradingName(viewModel);
+
+    //    // Assert
+    //    result.Should().BeOfType<ViewResult>();
+    //}
+
+    //[TestMethod]
+    //public async Task POST_GivenNoTradingName_ThenReturnViewWithUsersBadInput()
+    //{
+    //    // Arrange
+    //    const string badTradingName = "123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789";
+
+    //    _systemUnderTest.ModelState.AddModelError(nameof(TradingNameViewModel.TradingName), "Trading name must be 170 characters or less");
+    //    var viewModel = new TradingNameViewModel
+    //    {
+    //        TradingName = badTradingName
+    //    };
+
+    //    // Act
+    //    var result = await _systemUnderTest.TradingName(viewModel);
+
+    //    // Assert
+    //    result.Should().BeOfType<ViewResult>();
+    //    var viewResult = (ViewResult)result;
+
+    //    viewResult.Model.Should().BeOfType<TradingNameViewModel?>();
+    //    var resultViewModel = (TradingNameViewModel?)viewResult.Model;
+    //    resultViewModel!.TradingName.Should().Be(badTradingName);
+    //}
+
+    //[TestMethod]
+    //public async Task POST_GivenNoTradingName_ThenViewHasCorrectBackLink()
+    //{
+    //    // Arrange
+    //    _systemUnderTest.ModelState.AddModelError(nameof(TradingNameViewModel.TradingName), "Trading name field is required");
+
+    //    // Act
+    //    var result = await _systemUnderTest.TradingName(new TradingNameViewModel());
+
+    //    // Assert
+    //    result.Should().BeOfType<ViewResult>();
+
+    //    var viewResult = (ViewResult)result;
+
+    //    AssertBackLink(viewResult, PagePath.IsTradingNameDifferent);
+    //}
+}
