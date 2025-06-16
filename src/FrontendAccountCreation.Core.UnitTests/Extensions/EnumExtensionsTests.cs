e0ae84963c9dea2a4fba7b447c4b1139411d19e0
Ehsan.Hatami <ehsan.hatami@oceantg.com>
2025-06-16 17:33:36 +0100

    add more tests

--- src/FrontendAccountCreation.Core.UnitTests/Extensions/EnumExtensionsTests.cs
+++ src/FrontendAccountCreation.Core.UnitTests/Extensions/EnumExtensionsTests.cs
@@ -1,47 +1,77 @@
 ﻿using FluentAssertions;
 using FrontendAccountCreation.Core.Extensions;
-using System;
 
-namespace FrontendAccountCreation.Core.UnitTests.Extensions
+namespace FrontendAccountCreation.Core.UnitTests.Extensions;
+
+public enum Test
 {
-    public enum Test
+    [System.ComponentModel.Description("First Value")]
+    First,
+
+    [System.ComponentModel.Description("Second Value")]
+    Second,
+
+    Third // No description
+}
+
+[TestClass]
+public class EnumExtensionsTests
+{
+    [TestMethod]
+    [DataRow(Test.First, "First Value")]
+    [DataRow(Test.Second, "Second Value")]
+    [DataRow(Test.Third, "Third")]
+    public void GetDescription_ReturnsExpectedDescription(Test value, string expected)
     {
-        [System.ComponentModel.Description("First Value")]
-        First,
+        // Act
+        var description = value.GetDescription();
 
-        [System.ComponentModel.Description("Second Value")]
-        Second,
-
-        Third // No description
+        // Assert
+        description.Should().Be(expected);
     }
 
-    [TestClass]
-    public class EnumExtensionsTests
+    [TestMethod]
+    [DataRow(Test.First, "First Value")]
+    [DataRow(Test.Second, "Second Value")]
+    [DataRow(Test.Third, null)]
+    public void GetDescriptionOrNull_ReturnsExpectedDescription(Test value, string expected)
     {
-        [TestMethod]
-        [DataRow(Test.First, "First Value")]
-        [DataRow(Test.Second, "Second Value")]
-        [DataRow(Test.Third, "Third")]
-        public void GetDescription_ReturnsExpectedDescription(Test value, string expected)
-        {
-            // Act
-            var description = value.GetDescription();
+        // Act
+        var description = value.GetDescriptionOrNull();
 
-            // Assert
-            description.Should().Be(expected);
-        }
+        // Assert
+        description.Should().Be(expected);
+    }
 
-        [TestMethod]
-        [DataRow(Test.First, "First Value")]
-        [DataRow(Test.Second, "Second Value")]
-        [DataRow(Test.Third, null)]
-        public void GetDescriptionOrNull_ReturnsExpectedDescription(Test value, string expected)
-        {
-            // Act
-            var description = value.GetDescriptionOrNull();
+    [TestMethod]
+    public void GetDescription_WithInvalidEnumValue_ReturnsEnumToString()
+    {
+        Enum value = (Test)999;
+        var description = value.GetDescription();
+        description.Should().Be("999");
+    }
 
-            // Assert
-            description.Should().Be(expected);
-        }
+    [TestMethod]
+    public void GetDescriptionOrNull_WithInvalidEnumValue_ReturnsNull()
+    {
+        Enum value = (Test)999;
+        var description = value.GetDescriptionOrNull();
+        description.Should().BeNull();
+    }
+
+    [TestMethod]
+    public void GetDescriptionOrNull_WithNullEnum_ThrowsNullReferenceException()
+    {
+        Enum value = null;
+        Action act = () => value.GetDescriptionOrNull();
+        act.Should().Throw<NullReferenceException>();
+    }
+
+    [TestMethod]
+    public void GetDescription_WithNullEnum_ThrowsNullReferenceException()
+    {
+        Enum value = null;
+        Action act = () => value.GetDescription();
+        act.Should().Throw<NullReferenceException>();
     }
 }
