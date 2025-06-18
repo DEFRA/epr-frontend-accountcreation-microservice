095eadda85c088f6d0318c6ab789fc38e07bb739
Phil Davies <203141900+phil-davies-eviden@users.noreply.github.com>
2025-06-18 14:54:42 +0100

    wip

--- src/FrontendAccountCreation.Web/ViewModels/ReExAccount/BusinessAddressOverseasViewModel.cs
+++ src/FrontendAccountCreation.Web/ViewModels/ReExAccount/BusinessAddressOverseasViewModel.cs
@@ -8,27 +8,26 @@ public class BusinessAddressOverseasViewModel
 {
     //todo: reconcile with the errors in the story, e.g. invalid country name
     
-    [Required(ErrorMessage = "BusinessAddressOverseas.CountryNameError")]
-    [MaxLength(100, ErrorMessage = "BusinessAddressOverseas.CountryLengthError")]
+    [Required(ErrorMessage = "AddressOverseas.CountryNameError")]
+    [MaxLength(100, ErrorMessage = "AddressOverseas.CountryLengthError")]
     public string? Country { get; set; }
 
-    [Required(ErrorMessage = "BusinessAddressOverseas.AddressLine1NameError")]
-    [MaxLength(100, ErrorMessage = "BusinessAddressOverseas.AddressLine1LengthError")]
+    [Required(ErrorMessage = "AddressOverseas.AddressLine1Error")]
+    [MaxLength(100, ErrorMessage = "AddressOverseas.AddressLine1LengthError")]
     public string? AddressLine1 { get; set; }
 
-    [Required(ErrorMessage = "BusinessAddressOverseas.AddressLine2Error")]
-    [MaxLength(100, ErrorMessage = "BusinessAddressOverseas.AddressLine2LengthError")]
+    [MaxLength(100, ErrorMessage = "AddressOverseas.AddressLine2LengthError")]
     public string? AddressLine2 { get; set; }
 
-    [Required(ErrorMessage = "BusinessAddressOverseas.TownOrCityError")]
-    [MaxLength(70, ErrorMessage = "BusinessAddressOverseas.TownOrCityLengthError")]
+    [Required(ErrorMessage = "AddressOverseas.TownOrCityError")]
+    [MaxLength(70, ErrorMessage = "AddressOverseas.TownOrCityLengthError")]
     public string? TownOrCity { get; set; }
 
-    [MaxLength(50, ErrorMessage = "BusinessAddressOverseas.StateProvinceRegionLengthError")]
+    [MaxLength(50, ErrorMessage = "AddressOverseas.StateProvinceRegionLengthError")]
     public string? StateProvinceRegion { get; set; }
 
-    [Required(ErrorMessage = "BusinessAddressOverseas.PostcodeError")]
+    [Required(ErrorMessage = "AddressOverseas.PostcodeError")]
     [RegularExpression("^([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})$",
-        ErrorMessage = "BusinessAddressOverseas.PostcodeInvalidError")]
+        ErrorMessage = "AddressOverseas.PostcodeInvalidError")]
     public string? Postcode { get; set; }
 }
