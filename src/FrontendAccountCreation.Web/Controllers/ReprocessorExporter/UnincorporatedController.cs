2fed0d63f827c7baf77f36c83079bb1919c19069
Maksim Varkus <maksim.varkus@eviden.com>
2025-06-18 17:45:52 +0100

    Add unincorporated controller

---
+++ src/FrontendAccountCreation.Web/Controllers/ReprocessorExporter/UnincorporatedController.cs
@@ -0,0 +1,46 @@
+﻿using FrontendAccountCreation.Core.Sessions.ReEx;
+using FrontendAccountCreation.Web.Constants;
+using FrontendAccountCreation.Web.Controllers.Attributes;
+using FrontendAccountCreation.Web.Sessions;
+using FrontendAccountCreation.Web.ViewModels.ReExAccount;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontendAccountCreation.Web.Controllers.ReprocessorExporter;
+
+[Feature(FeatureFlags.AddOrganisationCompanyHouseDirectorJourney)]
+[Route("re-ex/organisation")]
+public class UnincorporatedController : ControllerBase<OrganisationSession>
+{
+    private readonly ISessionManager<OrganisationSession> _sessionManager;
+
+    public UnincorporatedController(ISessionManager<OrganisationSession> sessionManager) : base(sessionManager)
+    {
+        _sessionManager = sessionManager;
+    }
+
+    [HttpGet]
+    [Route(PagePath.UnincorporatedRoleInOrganisation)]
+    public async Task<IActionResult> RoleInOrganisation()
+    {
+        var session = await _sessionManager.GetSessionAsync(HttpContext.Session);
+
+        return View(new ReExRoleInOrganisationViewModel { Role = session.RoleInOrganisation });
+    }
+
+    [HttpPost]
+    [Route(PagePath.UnincorporatedRoleInOrganisation)]
+    public async Task<IActionResult> RoleInOrganisation(ReExRoleInOrganisationViewModel viewModel)
+    {
+        var session = await _sessionManager.GetSessionAsync(HttpContext.Session);
+
+        if (!ModelState.IsValid)
+        {
+            SetBackLink(session, PagePath.UnincorporatedRoleInOrganisation);
+            return View(viewModel);
+        }
+
+        session.RoleInOrganisation = viewModel.Role;
+
+        return await SaveSessionAndRedirect(session, nameof(RoleInOrganisation), PagePath.UnincorporatedRoleInOrganisation, null);
+    }
+}
---
+++ src/FrontendAccountCreation.Web/ViewModels/ReExAccount/ReExRoleInOrganisationViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FrontendAccountCreation.Web.ViewModels.ReExAccount;
+
+public class ReExRoleInOrganisationViewModel
+{
+    [Required(ErrorMessage = "ReExRoleInOrganisation.ErrorMessage.Required")]
+    [MaxLength(450, ErrorMessage = "ReExRoleInOrganisation.ErrorMessage.MaxLength")]
+    public string? Role {  get; set; }
+}
