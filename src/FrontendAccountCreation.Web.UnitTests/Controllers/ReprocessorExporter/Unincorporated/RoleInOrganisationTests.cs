9eb5ba6a714a75001f3e12d2fae59261e17244ec
Maksim Varkus <maksim.varkus@eviden.com>
2025-06-19 10:23:36 +0100

    Add tests

---
+++ src/FrontendAccountCreation.Web.UnitTests/Controllers/ReprocessorExporter/Unincorporated/RoleInOrganisationTests.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using FrontendAccountCreation.Core.Sessions.ReEx;
+using FrontendAccountCreation.Web.Constants;
+using FrontendAccountCreation.Web.ViewModels.ReExAccount;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace FrontendAccountCreation.Web.UnitTests.Controllers.ReprocessorExporter.Unincorporated;
+
+[TestClass]
+public class RoleInOrganisationTests : UnincorporatedTestBase
+{
+    private OrganisationSession _organisationSession = null!;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        SetupBase();
+
+        _organisationSession = new OrganisationSession
+        {
+            Journey = new List<string> { PagePath.UnincorporatedRoleInOrganisation }
+        };
+
+        _sessionManagerMock.Setup(x => x.GetSessionAsync(It.IsAny<ISession>())).ReturnsAsync(_organisationSession);
+    }
+
+    [TestMethod]
+    public async Task RoleInOrganisation_Get_ReturnsViewWithPrepopulatedModel()
+    {
+        // Arrange
+        var role = "test";
+        _organisationSession.RoleInOrganisation = role;
+
+        // Act
+        var result = await _systemUnderTest.RoleInOrganisation();
+
+        // Assert
+        var viewResult = result.Should().BeOfType<ViewResult>().Subject;
+        var model = viewResult.Model.Should().BeOfType<ReExRoleInOrganisationViewModel>().Subject;
+
+        model.Role.Should().Be(role);
+    }
+}
