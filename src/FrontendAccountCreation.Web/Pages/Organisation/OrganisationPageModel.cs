460aad47e2ad2c4eaa2aa3ca52542cf07df2e3d8
Phil Davies <203141900+phil-davies-eviden@users.noreply.github.com>
2025-07-01 12:43:24 +0100

    wip

---
+++ src/FrontendAccountCreation.Web/Pages/Organisation/OrganisationPageModel.cs
@@ -0,0 +1,57 @@
+﻿using FrontendAccountCreation.Core.Extensions;
+using FrontendAccountCreation.Core.Sessions.ReEx;
+using FrontendAccountCreation.Web.Sessions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Localization;
+
+namespace FrontendAccountCreation.Web.Pages.Organisation;
+
+public class OrganisationPageModel(
+    ISessionManager<OrganisationSession> sessionManager,
+    IStringLocalizer<SharedResources> sharedLocalizer)
+    : PageModel
+{
+    protected ISessionManager<OrganisationSession> SessionManager { get; } = sessionManager;
+    protected IStringLocalizer<SharedResources> SharedLocalizer { get; } = sharedLocalizer;
+
+    protected async Task<RedirectToActionResult> SaveSessionAndRedirect(
+        OrganisationSession session,
+        string actionName,
+        string currentPagePath,
+        string? nextPagePath)
+    {
+        session.IsUserChangingDetails = false;
+        await SaveSession(session, currentPagePath, nextPagePath);
+
+        return RedirectToAction(actionName);
+    }
+
+    private async Task SaveSession(
+        OrganisationSession session,
+        string currentPagePath,
+        string? nextPagePath)
+    {
+        var index = session.Journey.FindIndex(x => x != null && x.Contains(currentPagePath.Split("?")[0]));
+
+        // this also cover if current page not found (index = -1) then it clears all pages
+        session.Journey = session.Journey.Take(index + 1).ToList();
+
+        session.Journey.AddIfNotExists(nextPagePath);
+
+        AddPageToWhiteList(session, currentPagePath);
+        AddPageToWhiteList(session, nextPagePath);
+
+        await SessionManager.SaveSessionAsync(HttpContext.Session, session);
+    }
+
+    private void AddPageToWhiteList(
+        OrganisationSession session,
+        string currentPagePath)
+    {
+        if (!string.IsNullOrEmpty(currentPagePath))
+        {
+            session.WhiteList.Add(currentPagePath);
+        }
+    }
+}
--- src/FrontendAccountCreation.Web/Pages/Organisation/RegisteredAsCharity.cshtml.cs
+++ src/FrontendAccountCreation.Web/Pages/Organisation/RegisteredAsCharity.cshtml.cs
@@ -1,4 +1,3 @@
-using FrontendAccountCreation.Core.Extensions;
 using FrontendAccountCreation.Core.Sessions.ReEx;
 using FrontendAccountCreation.Web.Configs;
 using FrontendAccountCreation.Web.Constants;
@@ -9,64 +8,12 @@ using FrontendAccountCreation.Web.FullPages.Radios;
 using FrontendAccountCreation.Web.FullPages.Radios.Common;
 using FrontendAccountCreation.Web.Sessions;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Localization;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Options;
 using System.ComponentModel.DataAnnotations;
 using System.Net;
-using Microsoft.AspNetCore.Html;
 
 namespace FrontendAccountCreation.Web.Pages.Organisation;
-
-public class OrganisationPageModel(
-    ISessionManager<OrganisationSession> sessionManager,
-    IStringLocalizer<SharedResources> sharedLocalizer)
-    : PageModel
-{
-    protected ISessionManager<OrganisationSession> SessionManager { get; } = sessionManager;
-    protected IStringLocalizer<SharedResources> SharedLocalizer { get; } = sharedLocalizer;
-
-    protected async Task<RedirectToActionResult> SaveSessionAndRedirect(
-        OrganisationSession session,
-        string actionName,
-        string currentPagePath,
-        string? nextPagePath)
-    {
-        session.IsUserChangingDetails = false;
-        await SaveSession(session, currentPagePath, nextPagePath);
-
-        return RedirectToAction(actionName);
-    }
-
-    private async Task SaveSession(
-        OrganisationSession session,
-        string currentPagePath,
-        string? nextPagePath)
-    {
-        var index = session.Journey.FindIndex(x => x != null && x.Contains(currentPagePath.Split("?")[0]));
-
-        // this also cover if current page not found (index = -1) then it clears all pages
-        session.Journey = session.Journey.Take(index + 1).ToList();
-
-        session.Journey.AddIfNotExists(nextPagePath);
-
-        AddPageToWhiteList(session, currentPagePath);
-        AddPageToWhiteList(session, nextPagePath);
-
-        await SessionManager.SaveSessionAsync(HttpContext.Session, session);
-    }
-
-    private void AddPageToWhiteList(
-        OrganisationSession session,
-        string currentPagePath)
-    {
-        if (!string.IsNullOrEmpty(currentPagePath))
-        {
-            session.WhiteList.Add(currentPagePath);
-        }
-    }
-}
 
 public class RegisteredAsCharity : OrganisationPageModel, IRadiosPageModel
 {
