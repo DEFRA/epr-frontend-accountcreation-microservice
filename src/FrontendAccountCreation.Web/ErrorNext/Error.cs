9073c0cc5e514547640069cc620df95c4258e7d9
Phil Davies <203141900+phil-davies-eviden@users.noreply.github.com>
2025-07-03 09:31:18 +0100

    wip

--- src/FrontendAccountCreation.Web/ErrorNext/Error.cs
+++ src/FrontendAccountCreation.Web/ErrorNext/Error.cs
@@ -2,25 +2,6 @@
 
 namespace FrontendAccountCreation.Web.ErrorNext;
 
-public interface IError
-{
-    int Id { get; }
-    string Message { get; }
-    string HtmlElementId { get; }
-
-    /// <summary>
-    /// The id of the error message HTML element that is displayed next to the input control.
-    /// Will be used as the aria-describedby attribute value, when the input is in an errored state.
-    /// </summary>
-    string InputErrorMessageParaId { get; }
-
-    string FormGroupClass => "govuk-form-group--error";
-
-    string InputClass => "govuk-input--error";
-    string TextAreaClass => "govuk-textarea--error";
-    string SelectClass => "govuk-select--error";
-}
-
 public class ModelStateError
     : IError
 {
