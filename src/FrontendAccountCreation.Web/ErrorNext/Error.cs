a758ffc3eeb48ed80e589f0930e658cef20d3940
Phil Davies <203141900+phil-davies-eviden@users.noreply.github.com>
2025-07-03 09:32:59 +0100

    wip

--- src/FrontendAccountCreation.Web/ErrorNext/Error.cs
+++ src/FrontendAccountCreation.Web/ErrorNext/Error.cs
@@ -1,43 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
-
+﻿
 namespace FrontendAccountCreation.Web.ErrorNext;
-
-public class ModelStateError
-    : IError
-{
-    public ModelStateError(KeyValuePair<string, ModelStateEntry?> modelStateEntry)
-    {
-        //todo:
-        Id = 0; // ModelStateError does not have an ID, so we use 0.
-        //todo: what to throw?
-        //todo: localize here, rather than in the view for better testability
-        Message = modelStateEntry.Value.Errors.FirstOrDefault()?.ErrorMessage ?? throw new ArgumentNullException(nameof(modelStateEntry));
-        //HtmlElementId = modelStateEntry.Key;
-        //todo: store key in error, so can check by htmlelementid instead/as well as id?
-        //todo: for radios don't add value to first (if need way to link to first input)
-        HtmlElementId = modelStateEntry.Key; //.Replace(".", "_");
-        InputErrorMessageParaId = $"error-message-{HtmlElementId}";
-    }
-
-    //public string HtmlElementId
-    //{
-    //    get
-    //    {
-            
-    //        //if (errorState.ErrorIdToHtmlElementId == null)
-    //        //{
-    //        //    throw new InvalidOperationException($"ErrorIdToHtmlElementId is null. Set it on {nameof(ErrorState)}.");
-    //        //}
-
-    //        //return errorState.ErrorIdToHtmlElementId(Id);
-    //    }
-    //}
-
-    public int Id { get; }
-    public string Message { get; }
-    public string HtmlElementId { get; }
-    public string InputErrorMessageParaId { get; }
-}
 
 public class Error(PossibleError possibleError, ErrorState errorState)
     : IError
