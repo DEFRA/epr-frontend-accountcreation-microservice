36398e6f080d2bda6a0222a3c10661ecb0b70f8b
Phil Davies <203141900+phil-davies-eviden@users.noreply.github.com>
2025-07-03 14:39:07 +0100

    wip

--- src/FrontendAccountCreation.Web/ErrorNext/ErrorStateEmpty.cs
+++ src/FrontendAccountCreation.Web/ErrorNext/ErrorStateEmpty.cs
@@ -3,7 +3,7 @@
 // we could use one of the other error states as the empty state, but this is clearer
 public class ErrorStateEmpty : IErrorState
 {
-    public static ErrorStateEmpty Instance => new();
+    public static ErrorStateEmpty Instance { get; } = new();
 
     public bool HasErrors => false;
     public IEnumerable<IError> Errors => [];
