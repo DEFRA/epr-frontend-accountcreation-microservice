e4e8b7b8dc178829034efa0f94c52f63fcd5df06
Phil Davies <203141900+phil-davies-eviden@users.noreply.github.com>
2025-07-03 11:21:37 +0100

    remove unused wip

--- src/FrontendAccountCreation.Web/ErrorNext/PossibleError.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace FrontendAccountCreation.Web.ErrorNext;
-
-/// <summary>
-/// Represents a possible error that is displayed in the error summary and next to the input control.
-/// </summary>
-/// <param name="Id">The error id (usually the int representation of an enum of error types.)</param>
-/// <param name="ErrorMessage">The error message that is displayed in the error summary and next to the input control.</param>
-public record PossibleError(int Id, string ErrorMessage);
