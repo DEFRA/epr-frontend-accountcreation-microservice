dcb957e7b72da0e2ede68f0abb51554f1cfe58d2
Idris Abu <idris.abu@atos.net>
2025-07-07 21:40:10 +0100

    Made markup and resource file changes for pages 3a and 3b

--- src/FrontendAccountCreation.Web/Controllers/ReprocessorExporter/ApprovedPersonController.cs
+++ src/FrontendAccountCreation.Web/Controllers/ReprocessorExporter/ApprovedPersonController.cs
@@ -106,6 +106,7 @@ namespace FrontendAccountCreation.Web.Controllers.ReprocessorExporter
                 model.IsLimitedLiablePartnership = session.ReExCompaniesHouseSession?.Partnership?.IsLimitedLiabilityPartnership ?? false;
                 model.IsInEligibleToBeApprovedPerson = !IsEligibleToBeApprovedPerson(session);
                 model.IsNonUk = session.IsUkMainAddress == false;
+                model.IsUnincorporated = session.ReExManualInputSession.ProducerType == ProducerType.UnincorporatedBody;
 
                 string errorMessage = GetAddApprovedPersonErrorMessageKey(model);
 
