b9850278488667e0690dd58d1592fa30d9d8bee2
Phil Davies <203141900+phil-davies-eviden@users.noreply.github.com>
2025-07-07 11:44:54 +0100

    need to authorize for scopes when adding organisation

--- src/FrontendAccountCreation.Web/Controllers/ReprocessorExporter/OrganisationController.cs
+++ src/FrontendAccountCreation.Web/Controllers/ReprocessorExporter/OrganisationController.cs
@@ -899,6 +899,7 @@ public class OrganisationController : ControllerBase<OrganisationSession>
 
     [HttpGet]
     [Route(PagePath.DeclarationContinue)]
+    [AuthorizeForScopes(ScopeKeySection = ConfigKeys.FacadeScope)]
     public async Task<IActionResult> DeclarationContinue()
     {
         var session = await _sessionManager.GetSessionAsync(HttpContext.Session);
