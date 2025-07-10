e94d3f6cb9090720a2087b2ad065886a502ccbbd
Sharjeel Baloch <192210245+sharjeel-baloch-external@users.noreply.github.com>
2025-07-10 17:31:59 +0100

    Fixed page title, validation error, added some data checks

--- src/FrontendAccountCreation.Web/ViewModels/ReExAccount/TeamMemberViewModel.cs
+++ src/FrontendAccountCreation.Web/ViewModels/ReExAccount/TeamMemberViewModel.cs
@@ -1,8 +1,11 @@
-﻿using FrontendAccountCreation.Web.Controllers.Attributes;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using FrontendAccountCreation.Core.DataAnnotations;
+using FrontendAccountCreation.Web.Controllers.Attributes;
 
 namespace FrontendAccountCreation.Web.ViewModels.ReExAccount;
 
+[ExcludeFromCodeCoverage]
 public class TeamMemberViewModel
 {
     //to-do: shouldn't be required - if it's missing it should be a system error, rather than showing "The value '' is invalid." in the error summary section on the page
@@ -10,14 +13,18 @@ public class TeamMemberViewModel
     public Guid Id { get; set; }
 
     [Required(ErrorMessage = "TeamMemberDetails.EmailError")]
-    public string Email { get; set; }
+    [PublicEmailAddress(ErrorMessage = "TeamMemberDetails.EmailInvalidErrorMessage")]
+    public string? Email { get; set; }
 
-    [TeamMemberTelephoneNumberValidation(ErrorMessage = "TelephoneNumber.TelephoneNumberErrorMessage")]
-    public string Telephone { get; set; }
+    [Required(ErrorMessage = "TeamMemberDetails.TelephoneNumberErrorMessage")]
+    [TeamMemberTelephoneNumberValidation(ErrorMessage = "TeamMemberDetails.TelephoneNumberInvalidErrorMessage")]
+    public string? Telephone { get; set; }
 
     [Required(ErrorMessage = "TeamMemberDetails.FirstNameErrorMessage")]
+    [MaxLength(50, ErrorMessage = "TeamMemberDetails.FirstNameLengthErrorMessage")]
     public string? FirstName { get; set; }
 
     [Required(ErrorMessage = "TeamMemberDetails.LastNameErrorMessage")]
-    public string? LastName { get; set; }
+    [MaxLength(50, ErrorMessage = "TeamMemberDetails.LastNameLengthErrorMessage")]
+    public string? LastName { get; set; }    
 }
